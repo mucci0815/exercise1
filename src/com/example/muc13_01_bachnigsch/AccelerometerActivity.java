4260c1c9c4a32e86a60a1e90f4147961339e27c1
Martin <martin.bbach@gmail.com>
2013-04-28 16:32:58 +0200

    Accelerometer-Daten werden ausgelesen und als Text angezeigt

--- src/com/example/muc13_01_bachnigsch/AccelerometerActivity.java
+++ src/com/example/muc13_01_bachnigsch/AccelerometerActivity.java
@@ -1,14 +1,28 @@
 package com.example.muc13_01_bachnigsch;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.os.Build;
 
-public class AccelerometerActivity extends Activity {
+public class AccelerometerActivity extends Activity implements
+		SensorEventListener {
+
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private TextView text;
+	private TextView[] accText = new TextView[3];
+	private float[] gravity = new float[3];
+	private float[] acceleration = new float[3];
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +30,25 @@ public class AccelerometerActivity extends Activity {
 		setContentView(R.layout.activity_accelerometer);
 		// Show the Up button in the action bar.
 		setupActionBar();
+
+		// get TextViews
+		text = (TextView) findViewById(R.id.acc_text);
+		accText[0] = (TextView) findViewById(R.id.textAcc1);
+		accText[1] = (TextView) findViewById(R.id.textAcc2);
+		accText[2] = (TextView) findViewById(R.id.textAcc3);
+
+		// get sensormanager
+		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+		// get accelerometers
+		if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+			mAccelerometer = mSensorManager
+					.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+			text.setText("Found Sensor");
+		} else {
+			text.setText("No Accelerometer found!");
+		}
 	}
 
 	/**
@@ -50,6 +83,52 @@ public class AccelerometerActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mSensorManager.unregisterListener(this);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mSensorManager.registerListener(this, mAccelerometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		// In this example, alpha is calculated as t / (t + dT),
+		// where t is the low-pass filter's time-constant and
+		// dT is the event delivery rate.
+
+		final float alpha = (float) 0.4;
+
+		// Isolate the force of gravity with the low-pass filter.
+		gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+		gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+		gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+		// Remove the gravity contribution with the high-pass filter.
+		acceleration[0] = event.values[0] - gravity[0];
+		acceleration[1] = event.values[1] - gravity[1];
+		acceleration[2] = event.values[2] - gravity[2];
+		
+		// Display Values
+		accText[0].setText(Float.toString(acceleration[0]));
+		accText[1].setText(Float.toString(acceleration[1]));
+		accText[2].setText(Float.toString(acceleration[2]));
+		
+		
+
 	}
 
 }
