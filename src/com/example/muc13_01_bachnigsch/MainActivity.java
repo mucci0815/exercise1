62e742da5c5e8ffd6246f56060b148bbd03f978f
Max Nigsch <tarzan555@gmx.de>
2013-04-28 14:46:55 +0200

    Vier Activities erzeugt die durch Buttons aufrufbar sind

--- src/com/example/muc13_01_bachnigsch/MainActivity.java
+++ src/com/example/muc13_01_bachnigsch/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.muc13_01_bachnigsch;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +20,32 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    
+    /** Called when the user clicks the Light button */
+    public void callLight(View view) {
+    	Intent intent = new Intent(this, LightActivity.class);
+    	startActivity(intent);
+    }
+    
+    
+    /** Called when the user clicks the Accelometer button */
+    public void callAccelometer(View view) {
+    	Intent intent = new Intent(this, AccelerometerActivity.class);
+    	startActivity(intent);
+    }
+    
+    /** Called when the user clicks the Orientation button */
+    public void callOrientation(View view) {
+    	Intent intent = new Intent(this, OrientationActivity.class);
+    	startActivity(intent);
+    }
+    
+    /** Called when the user clicks the GPS button */
+    public void callGPS(View view) {
+    	Intent intent = new Intent(this, GPSActivity.class);
+    	startActivity(intent);
     }
     
 }
